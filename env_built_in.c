b7cf140cdd7635ad58e8d0eb4b91fa1a43c70e4a
mwahk3ymer <audreymwakima99@gmail.com>
2023-05-24 17:26:57 +0000

    environ

--- env_built_in.c
+++ env_built_in.c
@@ -9,13 +9,13 @@ extern char **environ;
 
 void printenv()
 {
-	char **env = environ;
+char **env = environ;
 	
-	while (*env != NULL)
-	{
-		printf("%s\n", *env);
-		env++;
-	}
+while (*env != NULL)
+{
+printf("%s\n", *env);
+env++;
+}
 }
 
 /**
@@ -26,30 +26,30 @@ void printenv()
 int main()
 {
 	
-	char prompt[] = " $ "; 
-	char command[MAX_INPUT_LENGTH]; 
+char prompt[] = " $ "; 
+char command[MAX_INPUT_LENGTH]; 
 
-	
-	while (1)
-	{
-		printf ("%s", prompt);
 
-		fgets(command, sizeof(command), stdin);
-        	command[strcspn(command, "\n")] = '\0';
-		if (strcmp(command, "env") == 0)
-       			{
-            			printenv();
-        		}	
-        else if (strcmp(command, "exit") == 0)
-        {
-            printf("Exiting shell...\n");
-            break;
-        }
-        else
-        {
-            printf("Unknown command: %s\n", command);
-        }
-    }
+while (1)
+{
+printf ("%s", prompt);
 
-    return 0;
+fgets(command, sizeof(command), stdin);
+command[strcspn(command, "\n")] = '\0';
+if (strcmp(command, "env") == 0)
+{
+printenv();
+}	
+else if (strcmp(command, "exit") == 0)
+{
+printf("Exiting shell...\n");
+break;
+}
+else
+{
+printf("Unknown command: %s\n", command);
+}
+}
+
+return 0;
 }
