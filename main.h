d9d03a0b48f07077eda98d73fbfe9cff718bf468
leahtugi <leahsparkle1@gmail.com>
2023-05-26 16:53:25 +0000

    final project

--- main.h
+++ main.h
@@ -1,21 +1,33 @@
-e#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
-ssize_t read_input(char **lineptr, size_t *n);
-char **tokenize_input(char *line, const char *delim, int *num_tokens);
-void free_arguments(char **args, int num_tokens);
-int main(int ac, char **argv);
-void execmd(char **argv);
-char *get_location(char *command);
-void printenv(void);
 
+extern char **environ;
+void _puts(char *str);
+void prompt(void);
+char *read_line(void);
+void _printenv(void);
+int _putchar(char h);
+int _strcmp(char *stringA, char *stringB);
+int c_atoi(char *l);
+void _kill(char *lineptr, char *tmp, char **tok);
+void ctrlc(int signum);
+int is_equal_delim(char b, const char *delim);
+char *_str_tokenization(char *sourc, const char *delim);
+char *_strcpy(char *dest, char *src);
+ssize_t get_line(char **str);
+char *_strdup(char *str);
 
 #endif
