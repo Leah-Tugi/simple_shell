6bb7360ea34b0c52362710da85b143b51120b283
mwahk3ymer <audreymwakima99@gmail.com>
2023-05-26 17:32:43 +0000

    new try

--- main.h
+++ main.h
@@ -13,21 +13,5 @@
 #include <dirent.h>
 #include <signal.h>
 
-extern char **environ;
-void prompt(void);
-int builtin(const char *str);
-void exits_sh(char, **tokens);
-int execmd(char, **tokens, char *args);
-int main(int ac, char *argv[]);
-void printenv(void);
-char *read_input(void);
-void _puts(char *str);
-size_t _stringlength(const char *s);
-char *_stringconcat(char *dest, const char *src);
-unsigned int _white_space(const char *s);
-char **_strto_tokens(const char *str);
-int exec_path(char **tokens, char *path, char *args);
-int check_path(char *s);
-char *path_builder(char **tokens);
-
+int main(void)
 #endif
---
+++ myshe.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+char buffer[BUFFER_SIZE];
+ssize_t chars;
+int status = 0;
+
+while (1)
+{
+printf("$ ");  /* Display prompt */
+
+chars = read(STDIN_FILENO, buffer, BUFFER_SIZE);  /* Read command line */
+if (chars == EOF)
+{
+printf("\n");
+break;  /* End of file, exit the shell */
+}
+
+if (chars > 0 && buffer[chars - 1] == '\n')
+buffer[chars - 1] = '\0';  /* Remove the newline character */
+
+if (access(buffer, X_OK) == 0)  /* Check if executable file exists */
+{
+pid_t child_pid = fork();
+if (child_pid == -1)
+{
+perror("Fork error");
+exit(EXIT_FAILURE);
+}
+
+if (child_pid == 0)  /* Child process */
+{
+if (execve(buffer, NULL, NULL) == -1)
+{
+perror("Execve error");
+exit(EXIT_FAILURE);
+}
+}
+else  /* Parent process */
+{
+wait(&status);
+}
+}
+else
+{
+fprintf(stderr, "Command not found: %s\n", buffer);
+}
+}
+
+return (0);
+}
